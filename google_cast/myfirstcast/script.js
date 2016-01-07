cd607ae75d83371c09c506e1ee5bee54a8d672e7
Gittermans <hannes_neukermans@hotmail.com>
2016-01-07 22:20:53 +0100

    no message

--- google_cast/myfirstcast/script.js
+++ google_cast/myfirstcast/script.js
@@ -2,7 +2,11 @@
     
     var mSession = null;
     
-    $(document).ready(function(){
+    $(document).ready(function() {
+        
+        $('#castme').click(function(){
+            loadMedia();
+        })
         
         if (!chrome.cast || !chrome.cast.isAvailable) {
             setTimeout(initializeCastApi, 1000);
@@ -46,7 +50,7 @@
                 
                 console.log('media session created with id: ' + e.sessionId)
                 
-                if( this.session.media.length != 0) {
+                if( mSession.media.length != 0) {
                     onMediaDiscovered('onRequestSessionSuccess', mSession.media[0]);                  
                 }
                 mSession.addMediaListener(onMediaDiscovered.bind(this, 'addMediaListener'));
@@ -78,6 +82,22 @@
         
         function onLaunchError(){
             console.log('onLaunchError');            
+        }
+        
+        function loadMedia(){
+            var mediaInfo = new chrome.cast.media.MediaInfo('http://weknowyourdreams.com/images/cat/cat-02.jpg', 'image/jpg'); 
+            mediaInfo.metadata = chrome.cast.media.MetadataType.PHOTO;
+            mediaInfo.metadata.metadataType = chrome.cast.media.MetadataType.PHOTO;
+            mediaInfo.contentType = 'image/jpg';
+            
+            var request = new chrome.cast.media.LoadRequest(mediaInfo);
+            mSession.loadMedia(request, onMediaDiscovered.bind(this, 'loadMedia'), onMediaError.bind(this));
+                       
+        }
+        
+        function onMediaError(e){
+            console.log('onMediaError:' + e);
+            
         }
         
         launchApp();
